5fd8732a49fdf62d0138eb9b49fc885b8426ef2e
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-03-03 15:48:44 +0100

    feat: setup basic goals page structure

--- src/components/goals/Goals.jsx
+++ src/components/goals/Goals.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import Goal from "../goal/Goal";
 
 function Goals() {
-  return <div>Goals</div>;
+  const [goalsList, setGoalsList] = useState([
+    {
+      title: "my first goal",
+      score: {
+        max: 5,
+        min: 0,
+      },
+    },
+  ]);
+
+  return (
+    <div className="goals">
+      Goals
+      {goalsList &&
+        goalsList.map((goal) => {
+          return <Goal goal={goal} />;
+        })}
+    </div>
+  );
 }
 
 export default Goals;
