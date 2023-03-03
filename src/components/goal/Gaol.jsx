4c582e759a49885884944a983e9ec5a2ffb42cbd
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-03-03 15:48:01 +0100

    refactor: rename Goal component due to typo

--- src/components/goal/Gaol.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Gaol() {
-  return <div>Gaol</div>;
-}
-
-export default Gaol;
---
+++ src/components/goal/Goal.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Goal({ goal }) {
+  return (
+    <div className="goal">
+      <p className="goal__title">{goal.title}</p>
+      <div className="score">
+        <button className="score__decrease">-</button>
+        <span className="score__actual">{goal.score.min}</span>
+        <span className="score__separator">/</span>
+        <span className="score__toReach">{goal.score.max}</span>
+        <button className="score__increase">+</button>
+      </div>
+    </div>
+  );
+}
+
+export default Goal;
