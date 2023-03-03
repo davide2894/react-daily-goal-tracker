a9ecccfb9d50da383902a05d8381233cb46630c2
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-03-03 12:35:59 +0100

    feat: homepage first draw

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Goals from "../goals/Goals";
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="home__welcomeMessage">
+        <p>Welcome to the goals app</p>
+        <p>Click on the button below to reach the goals page</p>
+      </div>
+      <Link path="goals" element={<Goals />}></Link>
+    </div>
+  );
 }
 
 export default Home;
