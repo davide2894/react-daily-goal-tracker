acfd50cb4fbfd50d12701278abb58b068a1bfc9c
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-03-06 11:44:02 +0100

    refactor: change index.js extension - doesn't need to be a jsx file

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import "./index.scss";
 import App from "./components/app/App";
 import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
+import { store } from "src/redux/redux-store";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HashRouter basename="/">
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </HashRouter>
   </React.StrictMode>
 );
