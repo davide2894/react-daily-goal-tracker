6f76cb5e0e17f1f179842a1b8c7ab6f495ab9c19
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-03-17 12:42:42 +0100

    feat: try to setup firebase

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
 import goalReducer from "./slices/goalSlice";
 import goalFormReducer from "./slices/goalFormSlice";
+import { firebaseReducer, actionTypes } from "react-redux-firebase";
+import { firestoreReducer } from "redux-firestore";
 
 export const store = configureStore({
   reducer: {
     goalReducer: goalReducer,
     goalFormReducer: goalFormReducer,
+    firebaseReducer: firebaseReducer,
+    firestoreReducer: firestoreReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [actionTypes.LOGIN, actionTypes.AUTH_LINK_ERROR],
+      },
+    }),
 });
