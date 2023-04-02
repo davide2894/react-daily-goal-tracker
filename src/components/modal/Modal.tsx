b397679148e4bc7d8b51c0bd37654e41779f5e54
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-04-02 21:27:46 +0200

    feat: handle close modal on click outside

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -5,6 +5,7 @@ function Modal(props) {
     function handleEscapeKey(event: KeyboardEvent) {
       if (event.code === "Escape") {
         props.onClose();
+        console.log("escape pressed");
       }
     }
     document.addEventListener("keydown", handleEscapeKey);
@@ -15,14 +16,26 @@ function Modal(props) {
     props.onClose();
   }
 
+  function closeModalOnClickOutside(evt) {
+    evt.stopPropagation();
+    evt.preventDefault();
+    evt.currentTarget.classList.contains("modalOverlay") && props.onClose();
+  }
+
   return (
-    <div className="goalForm editGoalForm modal modalOverlay">
+    <div
+      className="goalForm editGoalForm modal modalOverlay"
+      onClick={(evt) => closeModalOnClickOutside(evt)}>
       <button
         className="editGoalForm__close modalOverlay__closeButton"
         onClick={onCloseClick}>
         <span className="modalOverlay__closeIcon"></span>
       </button>
-      <div className="modalOverlay__main">
+      <div
+        className="modalOverlay__main"
+        onClick={(evt) => {
+          evt.stopPropagation();
+        }}>
         <div className="modalOverlay__content form">
           <h2 className="modalOverlay__h2">
             {props.mode === "add" ? "Add Goal" : "Edit Goal"}
