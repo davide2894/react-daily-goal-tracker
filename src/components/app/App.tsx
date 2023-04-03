2a4b3c0e0c72488eec70ab60397d6ba5c7fc75e6
Davide Iaiunese <davide.iaiunese.external@ynap.com>
2023-04-03 10:55:18 +0200

    fix: use createHashRouter for gh-pages

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -2,10 +2,10 @@ import "./App.scss";
 import Home from "../home/Home";
 import Goals from "../goals/Goals";
 import MyAccount from "../myAccount/MyAccount";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 
 function App() {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: "/",
       element: <Home />,
